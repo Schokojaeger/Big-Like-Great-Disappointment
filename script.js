69848b6d8a019e94dad609ccf016162718fcfc11
Anna-Maria <anna-maria.schlemmer@hse.com>
2024-01-29 11:27:55 +0100

    3 weeks school update

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function mainStory(story){
+  document.getElementById("story").textContent = story;
+}
+
+
+function buttonChangeOne(textOne){
+  document.getElementById("buttonOne").textContent = textOne;
+    }
+
+
+function buttonChangeTwo(textTwo){
+  document.getElementById("buttonTwo").textContent = textTwo;
+    }
+
+
+function buttonChangeThree(textThree){
+  document.getElementById("buttonThree").textContent = textThree;
+    }
+
+
+
+
