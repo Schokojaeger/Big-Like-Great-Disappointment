3c52b42cadc9a9af51b45ea1963dc353584c317a
tora9604 <schlemmer.anna@web.de>
2024-03-31 18:01:48 +0200

    changes on freegame

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--meta http-equiv="X-UA-Compatible" content="ie=edge"-->
     <title>Crush Time</title>
     <link rel="stylesheet" href="style.css">
 </head>
